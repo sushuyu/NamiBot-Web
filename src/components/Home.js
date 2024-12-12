954b2ac602e5fcf41c0932111cb06ecb8ea2dde1
MiaaaV <sirivesa@gmail.com>
2024-12-12 18:42:38 +0200

    layout changes and home.css added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import NavBar from '../components/Nav';
 import ChatWindow from '../components/ChatWindow';
+import '../styles/Home.css';
 
 const Home = () => {
   return (
     <div className="d-flex justify-content-center vh-100">
-      <div className='d-flex flex-column justify-content-center align-items-center'>
+      <div className='div-2 d-flex flex-column justify-content-center align-items-center'>
         <NavBar />
         <ChatWindow />
       </div>
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Outlet } from 'react-router-dom';
 import '../styles/Layout.css';
 import bgVideo from '../media/vecteezy_blue-ocean-underwater-with-sun-shine-background-loop_15472744.mp4';
 
 const Layout = () => {
+
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 375 && videoRef.current) {
+        videoRef.current.pause();
+      } else if (videoRef.current) {
+        videoRef.current.play();
+      }
+    };
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <div className="layout d-flex flex-column justify-content-center">
-      <video id='video' src={bgVideo} autoPlay loop muted />
-      <a id='attribution' href="https://www.vecteezy.com/free-videos/underwater-animation" target='_blank'>
+      <video id='video' ref={videoRef} src={bgVideo} autoPlay loop muted />
+      <a id='attribution' href="https://www.vecteezy.com/free-videos/underwater-animation" target='_blank' rel='noopener noreferrer'>
         Underwater Animation Stock Videos by Vecteezy
       </a>
       <div className="content">
